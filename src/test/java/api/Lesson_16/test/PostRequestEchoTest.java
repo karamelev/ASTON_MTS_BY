334052da0f3fef5398e2d9a152995d4bd35f69ab
karamelev <karamelev@tut.by>
2023-12-12 14:19:34 +0300

    Lesson_16

--- src/test/java/api/Lesson_16/test/PostRequestEchoTest.java
+++ src/test/java/api/Lesson_16/test/PostRequestEchoTest.java
@@ -1,14 +1,19 @@
 package api.Lesson_16.test;
 
 import api.Lesson_16.Specification;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostRequestEchoTest {
     private final static String URL = "https://postman-echo.com";
     @Test
-    public void postRequestTest() {
+    public void postFormDataRequestTest() {
         Specification.installSpecification(Specification.requestSpec(URL));
         given()
                     .basePath("/post")
@@ -18,8 +23,26 @@ public class PostRequestEchoTest {
                 .when()
                     .post()
                 .then().log().body()
+                    .contentType(ContentType.JSON)
                     .statusCode(200)
                     .body("form.foo1", equalTo("bar1"))
                     .body("form.foo2", equalTo("bar2"));
+    }
+
+    @Test
+    public void postRawTextRequestTest() {
+        Specification.installSpecification(Specification.requestSpec(URL));
+        Map<String,Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("text", "value");
+
+        given()
+                .basePath("/post")
+                .contentType(ContentType.JSON)
+                .body(jsonAsMap)
+                .when()
+                .post()
+                .then().log().all()
+                .statusCode(200)
+                .body("data.text", equalTo("value"));
     }
 }
